351e7abe7b95bccb72ac91d42acf6cfa476f9be6
Combir <r1029382x2gf@bk.ru>
2026-03-23 00:01:46 +0300

    Lab 4 done

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'pages/index.html')
+    context = {
+        'title': 'Главная — M School',
+        'welcome_message': 'Добро пожаловать в школу иностранных языков!',
+    }
+    return render(request, 'pages/index.html', context)
+
+def about(request):
+    context = {
+        'title': 'О школе — M School',
+    }
+    return render(request, 'pages/about.html', context)

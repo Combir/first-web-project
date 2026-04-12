d62d4d72684d881b72c2a772fe5184160d4be8da
Combir <r1029382x2gf@bk.ru>
2026-04-13 00:10:52 +0300

    Task 7 ready.

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='home'),
     path('about/', views.about, name='about'),
-]   
+    path('course/<int:pk>/', views.course_detail, name='course_detail'),
+]
